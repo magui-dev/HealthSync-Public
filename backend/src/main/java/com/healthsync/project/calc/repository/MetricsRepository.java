2efce6c90bb61a7aa588ff8eb1cd5e63c3fb86c4
hye <hyekyung0610@naver.com>
2025-09-10 16:34:38 +0900

    docs: MetricsRepository 메서드 설명 Docs 보완

--- backend/src/main/java/com/healthsync/project/calc/repository/MetricsRepository.java
+++ backend/src/main/java/com/healthsync/project/calc/repository/MetricsRepository.java
@@ -4,7 +4,7 @@ import com.healthsync.project.calc.domain.Metrics;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
- * CalcResult 엔티티의 CRUD 기능을 제공하는 JPA Repository 인터페이스.
+ * Metrics 엔티티의 CRUD 기능을 제공하는 JPA Repository 인터페이스.
  */
 
 public interface MetricsRepository extends JpaRepository<Metrics, Long> { }
