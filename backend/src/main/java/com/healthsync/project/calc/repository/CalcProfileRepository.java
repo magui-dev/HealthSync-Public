028156408b460166e10a322be5087f88e4936f95
hye <hyekyung0610@naver.com>
2025-09-10 16:32:53 +0900

    feat(calc/repository): Optional 추가 및 Docs 수정

--- backend/src/main/java/com/healthsync/project/calc/repository/CalcProfileRepository.java
+++ backend/src/main/java/com/healthsync/project/calc/repository/CalcProfileRepository.java
@@ -3,8 +3,12 @@ package com.healthsync.project.calc.repository;
 import com.healthsync.project.calc.domain.CalcProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 /**
- * TestUser 엔티티의 CRUD 기능을 제공하는 JPA Repository 인터페이스.
+ * CalcProfile 엔티티의 CRUD 기능을 제공하는 JPA Repository 인터페이스.
  */
 
-public interface CalcProfileRepository extends JpaRepository<CalcProfile, Long> { }
+public interface CalcProfileRepository extends JpaRepository<CalcProfile, Long> {
+    Optional<CalcProfile> findByEmail(String email);
+}
